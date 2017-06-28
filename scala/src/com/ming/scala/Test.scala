a2f32b270f4d69b38b3e8f14a2caa21dd72182cf
CrazyTechnology <limingming_0305@163.com>
2017-06-28 17:21:18 +0800

    wordcount

---
+++ scala/src/com/ming/scala/Test.scala
@@ -0,0 +1,12 @@
+package com.ming.scala
+
+/**
+  * Created by root on 6/28/17.
+  */
+class Test {
+  def  sayHello(name:String,age:String)={
+    print("hello"+name+"welcome to our word even you are "+age+" years old!!");
+    name+age;
+  }
+
+}
