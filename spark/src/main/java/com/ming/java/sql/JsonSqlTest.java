33ed1fc0200aebacc3c48102cbf40e8116fe5f9a
CrazyTechnology <limingming_0305@163.com>
2017-07-17 10:28:06 +0800

    spark project

--- spark/src/main/java/com/ming/java/sql/JsonSqlTest.java
+++ spark/src/main/java/com/ming/java/sql/JsonSqlTest.java
@@ -1,10 +1,18 @@
 package com.ming.java.sql;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SQLContext;
-
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 /**
  * Created by Administrator on 2017-07-17.
  */
@@ -16,7 +24,8 @@ public class JsonSqlTest {
         JavaSparkContext sc = new JavaSparkContext(conf);
         SQLContext sqlContext = new SQLContext(sc);
         // 针对json文件，创建DataFrame（针对json文件创建DataFrame）
-        DataFrame studentScoresDF = sqlContext.read().json(
-                "hdfs://spark1:9000/spark-study/students.json");
+        DataFrame studentScoresDF = sqlContext.jsonFile(
+                "hdfs://nns:9000/spark-study/students.json");
+
     }
 }
