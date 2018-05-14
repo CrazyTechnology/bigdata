9903f73355ed743a41627857505ad8b3dd1be527
limingming <limingming_0305@163.com>
2018-05-14 15:19:41 +0800

    修改项目

--- scala/src/main/java/com/ming/scala/test/traitTest.scala
+++
@@ -1,9 +0,0 @@
-package com.ming.scala.test
-
-/**
-  * Created by root on 6/30/17.
-  */
-trait traitTest {
-
-
-}
--- spark-data-analyse/src/main/java/com/ming/bigdata/test/Test.java
+++
@@ -1,8 +0,0 @@
-package com.ming.bigdata.test;
-
-/**
- * Created by root on 7/8/17.
- * ce shi
- */
-public class Test {
-}
