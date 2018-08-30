fd860ae17285b6f919e3af7ff40f94bd13a4ec54
limingming <limingming_0305@163.com>
2018-08-30 15:29:01 +0800

    flume 拦截器

--- integrated-project/src/main/java/test/TestUtils.java
+++ integrated-project/src/main/java/test/TestUtils.java
@@ -8,10 +8,9 @@ import org.junit.Test;
  * Created by ming on 18-6-19.
  */
 public class TestUtils {
+
     @Test
     public void testProperties(){
-        String mysql = PropertiesUtils.getProperties("mysql", "resource.properties");
-        StringUtils.isEmpty(mysql);
-        System.out.println(mysql);
+
     }
 }
