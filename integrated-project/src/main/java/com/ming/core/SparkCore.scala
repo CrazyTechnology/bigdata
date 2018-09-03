12fe046652889bce9424e3366118e71df1a76131
limingming <limingming_0305@163.com>
2018-09-03 19:20:10 +0800

    flume 拦截器

--- integrated-project/src/main/java/com/ming/core/SparkCore.scala
+++ integrated-project/src/main/java/com/ming/core/SparkCore.scala
@@ -4,10 +4,13 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkCore {
   def main(args: Array[String]): Unit = {
-    val conf= new SparkConf().setAppName("Test").setMaster("local")
-    val sc=new SparkContext(conf)
-    val list=Array("hello","word","my","name","is","my")
-    val rdd=sc.parallelize(list).map(x=>(x,1))
-    rdd.reduceByKey(_+_).collect().foreach(print(_))
+    val conf = new SparkConf().setAppName("Test").setMaster("local")
+    val sc = new SparkContext(conf)
+    val list = Array("hello", "word", "my", "name", "is", "my")
+    val rdd = sc.parallelize(list).map(x => (x, 1))
+   val map= rdd.countByKey()
+   val array= rdd.top(3)
+   val value= map.get("my").getOrElse("")
+    print(rdd.count())
   }
 }
